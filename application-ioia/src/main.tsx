ff72b86e17092827e8e0916496155968e33908af
lorenzobruni-dev <109805268+lorenzobruni-dev@users.noreply.github.com>
2024-10-24 12:32:40 +0200

    Add Chakra to the project

--- application-ioia/src/main.tsx
+++ application-ioia/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { Provider } from "../src/components/ui/provider"
 import './index.css'
+import ReactDOM from "react-dom/client"
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider>
+    <App /> 
+
+    </Provider>
   </StrictMode>,
 )
